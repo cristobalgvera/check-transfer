6700d2107e03055e38ab7a22b239c545a6d96792
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-09-13 22:50:36 -0300

    refactor(project): standardize selector prefix
    all shared libs will start with `app`, all project specific selectors will start with `<project-name>-app`

--- libs/shared-ui/src/lib/navbar/navbar.component.ts
+++ libs/shared-ui/src/lib/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Link } from './model/link.model';
 
 @Component({
-  selector: 'check-navbar',
+  selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
